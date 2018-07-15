73bcd505e043887d2d50bd8939a2362b65979c73
Krishan <kgs.121001@gmial.com>
2018-07-16 01:28:02 +0530

    swipe crash solved

--- app/src/main/java/com/example/krishan/travels/Review.java
+++ app/src/main/java/com/example/krishan/travels/Review.java
@@ -2,13 +2,26 @@ package com.example.krishan.travels;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Review extends AppCompatActivity {
 
+    Button button;
+    String data;
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+        et=findViewById(R.id.r);
+        data=et.getText().toString();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
     }
 }
